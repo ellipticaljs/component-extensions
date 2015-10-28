1456cadb7b2cd29a19472e52ee86a66425efd6ac
Scott <dev@misonline.biz>
2015-10-28 07:24:06 -0400

    Updates

--- dist/component.extensions.js
+++ dist/component.extensions.js
@@ -834,28 +834,3 @@
 
     };
 }));
-
-//umd pattern
-
-(function (root, factory) {
-    if (typeof module !== 'undefined' && module.exports) {
-        //commonjs
-        module.exports = factory(require('elliptical-utils'),require('jquery-extensions'),require('./element'),
-            require('./dataStore'),require('./device'), require('./template'),
-            require('./transition'), require('./transform'),require('./utils'),require('./event'));
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['elliptical-utils','jquery-extensions','./prototype.element','./prototype.dataStore','./prototype.device',
-            './prototype.template','./prototype.transition','./prototype.transform','./prototype.utils','./prototype.event'], factory);
-    } else {
-        // Browser globals (root is window)
-        var e=root.elliptical.prototype.extensions;
-        root.elliptical.extensions.mixin = factory(root.elliptical.utils,root,e.element,e.dataStore,e.device,
-            e.template,e.transition,e.transform,e.utils,e.event);
-        root.returnExports = root.elliptical.extensions.mixin;
-    }
-}(this, function (utils,root,element,dataStore,device,template,transition,transform,util,event) {
-
-    return Object.assign({},element,dataStore,util,device,template,transition,transform,event);
-
-}));
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+
+
+module.exports=require('jquery-extensions');
+module.exports.dust=require('./dist/dust');
+module.exports.utils=require('./dist/elliptical.utils');
+module.exports.moment=require('moment');
+module.exports.mutationSummary=require('./dist/mutation-summary');
+module.exports.extensions={
+    element:require('./lib/prototype.element'),
+    dataStore:require('./lib/prototype.dataStore'),
+    device:require('./lib/prototype.device'),
+    utils:require('./lib/prototype.utils'),
+    drawer:require('./lib/prototype.drawer'),
+    navigation:require('./lib/prototype.navigation'),
+    transition:require('./lib/prototype.transition'),
+    transform:require('./lib/prototype.transform'),
+    template:require('./lib/prototype.template'),
+    event:require('./lib/prototype.event'),
+    base:require('./lib/prototype.base')
+};
+

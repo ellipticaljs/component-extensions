2dbf3fcc0a0d685eec4303a13df09bfbdc5f63ca
Scott <dev@misonline.biz>
2015-11-06 09:15:28 -0500

    Added prototype.options

--- bundle/component.extensions.js
+++ bundle/component.extensions.js
@@ -16866,3 +16866,34 @@ var MutationSummary = (function () {
 
     };
 }));
+
+//umd pattern
+
+(function (root, factory) {
+    if (typeof module !== 'undefined' && module.exports) {
+        //commonjs
+        module.exports = factory(require('elliptical-utils'));
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['elliptical-utils'], factory);
+    } else {
+        // Browser globals (root is window)
+        root.elliptical.extensions=root.elliptical.extensions || {};
+        root.elliptical.extensions.options = factory(root.elliptical.utils);
+        root.returnExports = root.elliptical.extensions.options;
+    }
+}(this, function (utils) {
+
+    return {
+
+        _setOptionsFromAttribute:function(){
+            var options=(this.options) ? this.options.opts : this.opts;
+            if(options===undefined) return;
+            options=JSON.parse(options);
+            var src=(this.options) ? this.options : this;
+            Object.assign(src,options);
+        }
+
+
+    };
+}));
